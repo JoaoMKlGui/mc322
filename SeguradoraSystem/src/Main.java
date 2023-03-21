7702a3e5e2b26b5eb9815dba09c1af0fbb2c009b
JoaoMKlGui <j260542@dac.unicamp.br>
2023-03-21 10:41:41 -0300

    atualizando a main com uns mini testes

--- SeguradoraSystem/src/Main.java
+++ SeguradoraSystem/src/Main.java
@@ -1,10 +1,26 @@
 public class Main {
     public static void main(String[] args) {
 
-        Cliente c1 = new Cliente("joao", "527.903.908-00", "31/12/2003", 19, "jacarandas");
+        Cliente clienteNovo = new Cliente("joao", "092.855.859-22", "15/12/2004", 18, "jacarandas");
 
-        System.out.println(c1.validarCPF());
-        System.out.println(c1.toString());
+        System.out.println(clienteNovo.validarCPF());
+        System.out.println(clienteNovo.toString());
+
+        Sinistro sinistroNovo = new Sinistro("21/03/2023", "princesa d'oeste");
+
+        System.out.println(sinistroNovo.getData()); 
+        System.out.println(sinistroNovo.getID()); 
+        System.out.println(sinistroNovo.getEndereco());
+        
+        Seguradora seguradoraNova = new Seguradora("seguradora do bairro", "(19) 91111-1111", "seguradora@gmail.com", "unicamp");
+
+        System.out.println(seguradoraNova.getEmail());
+
+        Veiculo carro = new Veiculo("AAA-0000", "ford", "shelby");
+
+        System.out.println(carro.getModelo());
+
+
     }
 
     
